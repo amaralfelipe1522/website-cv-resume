ddcbf5913332263e0ff022e944d54d9b150968bc
amaralfelipe1522 <amaral.felipe@live.com>
2020-03-26 14:14:42 -0300

    Adding port variable

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
 	fs := http.FileServer(http.Dir("public"))
 	http.Handle("/", fs)
 
 	log.Println("Executando na porta 3000...")
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
